e784b26c82cac5e8e0dc69aacaf0cc802851a1c0
victor2996 <vicctor96@ukr.net>
2021-01-25 21:35:31 +0200

    Header is Finished

--- Theater/js/index.js
+++ Theater/js/index.js
@@ -1,7 +1,26 @@
+// const panel = document.getElementsByClassName('.navbar')[0];
+// window.addEventListener('scroll', function () {
+//     if (pageYOffset > 100) {
+//         panel.classList.add('navbar-active');
+//     } else {
+//         panel.classList.remove('navbar-active');
+//     }
+// });
+
 $(document).ready(function () {
-    $('.navbar__burger').click(function (event) {
-        $('.navbar__burger, .navbar__menu').toggleClass('active');
-        $('body').toggleClass('lock');
+    $(window).scroll(function () {
+        if (pageYOffset > 100) {
+            $('.navbar').addClass('navbar-active');
+        } else {
+            $('.navbar').removeClass('navbar-active');
+        }
     });
 });
+
+// $(document).ready(function () {
+//     $('.navbar__burger').click(function (event) {
+//         $('.navbar__burger, .navbar__menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     });
+// });
 ;
